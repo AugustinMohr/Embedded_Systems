da1af626d9bd141b6a606d7f0dd21a4c6ea0c4ce
Augustin Mohr <augustin.mohr@epfl.ch>
2020-12-18 14:09:06 +0100

    Added Vhdl component file

---
+++ lab_4_project_template/hw/hdl/LT24_controller.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+entity Segment_7 is
+
+	port(
+	
+		clk		: in std_logic;
+		nReset	: in std_logic;
+		
+		
+		--Internal interface (i.e. Avalon slave).
+		
+		--External interface (i.e. conduit)
+		
+	);
+end Segment_7;
+
+architecture comp of Segment_7 is
+
+
+signal 
+
+
+
+begin
+
+	
+end comp;	
+					

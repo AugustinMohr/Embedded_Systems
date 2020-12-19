b3669c0e0461c69eb6c5175c50b26037362bd44a
NicolasCvdn <nic.cavedon@gmail.com>
2020-12-19 13:41:14 +0100

    No idea what Im doing

---
+++ lab_4_project_template/hw/hdl/Avalon_Master.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+entity Avalon_Master is
+
+	port(
+	
+		clk		: in std_logic;
+		nReset	: in std_logic;
+		
+		
+		--Internal interface (i.e. Avalon slave).
+		
+		--External interface (i.e. conduit)
+		
+	);
+end Avalon_Master;
+
+architecture comp of Avalon_Master is
+
+
+signal 
+
+
+
+begin
+
+	
+end comp;	
+					

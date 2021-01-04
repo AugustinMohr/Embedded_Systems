5605b5ccf2f53ebdbc6917837ec129f46d292917
NicolasCvdn <nic.cavedon@gmail.com>
2021-01-04 14:58:24 +0100

    push

--- lab_4_project_template/sw/nios/application/main.c
+++ lab_4_project_template/sw/nios/application/main.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <ILI9341.h>
+#include "system.h"
+#include "io.h"
+
+#define AS 
+#define AS_CS
+#define AS_WRITE
+#define AS_WRITEDATA
 
 
+char* filename = "/mnt/host/image.ppm"
+FILE *foutput = fopen(filename, "w");
 
-char* filename = "/mnt/host/image.ppm";
+if (!foutput) {
+ printf("Error: could not open \"%s\" for writing\n", filename);
+ return false;
+}
 
-
-
-int main()
-{
-  LCD_Init();
-
-  while(1)
-  {
-
+/* Use fprintf function to write to file through file pointer */
+fprintf(foutput, "Writing text to file\n");
 
 
-  }
+int main() {
+	LCD_Init();
+	buffer_address = 0;
+	buffer_length = 0;
 
+	// default LCD initialisation commands
+
+	// Give the buffer address and buffer length 
+	IOWR_DIRECT32(AS, AS_CS, 1);
+	IOWR_DIRECT32(AS, AS_WRITEDATA, buffer_address);
+	IOWR_DIRECT32(AS, AS_ADDRESS, BUFFER_ADDRESS);	
+	IOWR_DIRECT32(AS, AS_WRITE, 1);
+
+	IOWR_DIRECT32(AS, AS_ADDRESS, BUFFER_LENGTH);
+	IOWR_DIRECT32(AS, AS_WRITEDATA, buffer_length);
+	// Set the 
+  	while(1) {
+  	}
 }

5dddc1461f3a7198bb5f5dd73a1b715402518928
NicolasCvdn <nic.cavedon@gmail.com>
2021-01-05 10:16:43 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ lab_4_project_template/hw/quartus/software-Copy/labo4/main.c
@@ -0,0 +1,42 @@
+/*
+ * "Hello World" example.
+ *
+ * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
+ * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
+ * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
+ * device in your system's hardware.
+ * The memory footprint of this hosted application is ~69 kbytes by default
+ * using the standard reference design.
+ *
+ * For a reduced footprint version of this template, and an explanation of how
+ * to reduce the memory footprint for a given application, see the
+ * "small_hello_world" template.
+ *
+ */
+
+#include <stdio.h>
+#include <io.h>
+#include "ILI9341.h"
+#include "terasic_includes.h"
+
+// defines
+#define LCD_CONTROLLER_0_BASE   0x0
+#define BUFFER_ADDRESS_OFFSET   (4 * 0b0000)
+#define BUFFER_LENGTH_OFFSET    (4 * 0b0001)
+#define LCD_COMMAND_OFFSET      (4 * 0b0010)
+#define LCD_DATA_OFFSET         (4 * 0b0011)
+
+#define PIO_LEDS_BASE 0x10000810
+
+int main(void)
+{
+    printf("start:\n");
+    LCD_init();
+    LCD_Clear(0X0000);
+    //IOWR_32DIRECT(LCD_CONTROLLER_0_BASE, LCD_COMMAND_OFFSET, 0x0001);
+    IOWR_8DIRECT(PIO_LEDS_BASE, 1, 0x00);
+
+
+    LCD_Pattern_Horizon();
+    return 0;
+}
---
+++ lab_4_project_template/hw/quartus/software-Copy/labo4/obj/default/main.d
@@ -0,0 +1,6 @@
+obj/default/main.o: main.c ../labo4_bsp//HAL/inc/io.h \
+ ../labo4_bsp//HAL/inc/alt_types.h
+
+../labo4_bsp//HAL/inc/io.h:
+
+../labo4_bsp//HAL/inc/alt_types.h:
